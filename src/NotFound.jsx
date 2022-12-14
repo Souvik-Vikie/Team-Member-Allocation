822cebb8799e89d25f3f1d55f8ac9aae5a69e067
SouvikCode003 <>
2022-12-14 17:27:32 +0000

    Initial commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound=()=>{
+  return(
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+         <h3 className="text-danger">404-Page Not Found</h3>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default NotFound;
